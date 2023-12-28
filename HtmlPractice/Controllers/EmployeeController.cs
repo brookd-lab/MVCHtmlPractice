37ade49f86a3c46cbc14caeed1ee527b49c2df86
David Brook <brookd2022@gmail.com>
2023-12-27 17:43:41 -0800

    Add project files.

---
+++ HtmlPractice/Controllers/EmployeeController.cs
@@ -0,0 +1,83 @@
+﻿using HtmlPractice.Data;
+using HtmlPractice.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HtmlPractice.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var employees = await _context.Employees.ToListAsync();
+            return View(employees);
+        }
+
+        private async Task<Employee> GetEmployeeById(int Id)
+        {
+            var employee = await _context.Employees.FindAsync(Id);
+            return employee;
+        }
+
+        public async Task<IActionResult> Details(int Id)
+        {
+            var employee = await GetEmployeeById(Id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        public IActionResult Create()
+        {
+            var employee = new Employee();
+            return View(employee);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Employee employee)
+        {
+            await _context.AddAsync(employee);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Update(int Id)
+        {
+            var employee = await GetEmployeeById(Id);
+            return View(employee);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Employee employee)
+        {
+            _context.Update(employee);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(int Id)
+        {
+            var employee = await GetEmployeeById(Id);
+            return View(employee);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Employee employee)
+        {
+            _context.Remove(employee);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ HtmlPractice/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using HtmlPractice.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HtmlPractice.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options) { }
+        public DbSet<Employee>? Employees { get; set; }
+    }
+    
+}
